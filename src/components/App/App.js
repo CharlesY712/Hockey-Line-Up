39ab25c28ba787dfd6fd7882c2d10aa02accfdd7
Charles Yach <CharlesY712@gmail.com>
2018-04-08 15:08:23 -0600

    Setting up more file architecture

---
+++ src/components/App/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from '../Header/Header';
+import Home from '../Home/Home';
+import Day from '../Day/Day';
+import Week from '../Week/Week';
+import Month from '../Month/Month';
+
+import './App.css';
+
+class App extends Component {
+  
+  
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route
+            exact path="/"
+            component={Home}
+          />
+          <Route
+            path="/day"
+            component={Day}
+          />
+          <Route
+            path="/week"
+            component={Week}
+          />
+          <Route
+            path="/month"
+            component={Month}
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Month/Month.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Month = () => {
+  return (
+    <div>MONTH</div>
+  );
+};
+
+export default Month;
