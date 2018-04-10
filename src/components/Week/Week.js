95f6b54384492e36e66f70262e8ed5a99040009a
Charles Yach <CharlesY712@gmail.com>
2018-04-09 23:51:13 -0600

    Adding dayCleaner to helpers

--- src/components/Week/Week.js
+++ src/components/Week/Week.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Week = () => {
   return (
-    <div>Week</div>
+    <div>Please select a week in the box above.</div>
   );
 };
 
--- src/helpers/dayCleaner.js
+++ src/helpers/dayCleaner.js
@@ -1,6 +1,11 @@
-
-
-export const dayCleaner = (season, day) => {
-  const games = season.gameentry.reduce()
-
+const dayCleaner = (season, day) => {
+  const games = season.reduce((gamesOnDay, game) => {
+    if (game.date === day) {
+      gamesOnDay.push(game);
+    }
+    return gamesOnDay;
+  }, []);
+  return games;
 };
+
+export default dayCleaner;
