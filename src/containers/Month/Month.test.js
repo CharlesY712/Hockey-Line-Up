f67b9ab0db8d7191d913da334f81f6b087c341ab
Charles Yach <CharlesY712@gmail.com>
2018-04-17 14:40:06 -0600

    Adding  tests for month
    Month tests passing at 100%

--- src/containers/Month/Month.test.js
+++ src/containers/Month/Month.test.js
@@ -1,11 +1,88 @@
-import { mapStateToProps, mapDispatchToProps } from './Month';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Month, mapStateToProps, mapDispatchToProps } from './Month';
 import { schedule }  from '../../mocks/mockSchedule';
+import { fetchSeason } from '../../helpers/fetchSeason';
 import * as actions from '../../actions';
+
+jest.mock('../../helpers/fetchSeason.js');
+jest.mock('../../helpers/fetchScoreboard.js');
 
 describe('Month', () => {
 
   describe('Month container', () => {
+    const mockAddSchedule = jest.fn();
+    const mockSetDate = jest.fn();
+    let wrapper;
 
+    it('should not display schedule if there are no games in Month', () => {
+      wrapper = shallow(<Month
+        date={'2018-06'}
+        schedule={schedule.dailygameschedule.gameentry}
+        addSchedule={mockAddSchedule}
+        setDate={mockSetDate}
+      />);
+      
+      const mockPrevProps = {date: '2018-04'};
+      wrapper.instance().componentDidUpdate(mockPrevProps);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should not display games on mount if selected date length is not 7', () => {
+      wrapper = shallow(<Month
+        date={'2018-04-12'}
+        schedule={[schedule]}
+        addSchedule={mockAddSchedule}
+        setDate={mockSetDate}
+      />);
+      
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should not display games on update if selected date length is not 7', () => {
+      wrapper = shallow(<Month
+        date={'2018-04-12'}
+        schedule={[schedule]}
+        addSchedule={mockAddSchedule}
+        setDate={mockSetDate}
+      />);
+      
+      const mockPrevProps = {date: '2018-04-14'};
+      wrapper.instance().componentDidUpdate(mockPrevProps);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should display schedule when a month is selected', () => {
+      wrapper = shallow(<Month
+        date={'2018-04'}
+        schedule={schedule.dailygameschedule.gameentry}
+        addSchedule={mockAddSchedule}
+        setDate={mockSetDate}
+      />);
+      
+      expect(fetchSeason).toHaveBeenCalled();
+    });
+
+    it('should display schedule when the component is updated', () => {
+      wrapper = shallow(<Month
+        date={'2018-04'}
+        schedule={schedule.dailygameschedule.gameentry}
+        addSchedule={mockAddSchedule}
+        setDate={mockSetDate}
+      />);
+
+      const mockPrevProps = {date: '2018-03'};
+      wrapper.instance().componentDidUpdate(mockPrevProps);
+
+      expect(fetchSeason).toHaveBeenCalled();
+    });
+
+    it('should display a loading gif if state.isLoading is true', () => {
+      wrapper.setState({isLoading: true});
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 
   describe('mapStateToProps', () => {
@@ -19,7 +96,7 @@ describe('Month', () => {
     });
 
     it('correctly maps the date to props', () => {
-      const date = '2018-04-12';
+      const date = '2018-04';
       const mockState = { setDate: date };
       const mapped = mapStateToProps(mockState);
       const expected = date;
